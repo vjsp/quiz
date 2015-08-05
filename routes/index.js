aa795dbeff91f24c067b39a1531e212394c0d401
vjsp <vj.sanchezp@gmail.com>
2015-08-05 02:37:09 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,9 @@ router.get('/', function(req, res) {
 // Página de autor
 router.get('/author', quizController.credits);
 
-// Autoload de comandos con :quizId
+// Autoload de comandos
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
@@ -34,5 +35,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
